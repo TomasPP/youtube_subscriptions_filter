32c234b349b27061bf181c2d949c66bedaeb3807
TomasPP <ltbrfree@gmail.com>
2020-05-12 19:46:54 +0300

    fixing errors

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author_email="ttt@ttt.tt",
     url="...",
     py_modules=["subscriptions_filter"],
-    install_requires=["google-api-python-client>=1.6.7,<1.7.0", "python-dateutil>=2.7.5,<3", "jsonpath-ng>=1.5,<2",
+    install_requires=["google-api-python-client>=1.8.2,<=2.0.0", "python-dateutil>=2.7.5,<3", "jsonpath-ng>=1.5,<2",
                       "requests>=2.23,<3", "httplib2>=0.17.3,<1"],
     entry_points={"console_scripts": ["subscriptions_filter=subscriptions_filter:main"]},
 )
